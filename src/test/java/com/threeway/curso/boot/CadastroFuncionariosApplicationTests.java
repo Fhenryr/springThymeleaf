eda3a3e631418e2e707c9d87aedb42f00fff4a2e
Fhenryr <fhenryr.mc@gmail.com>
2019-09-30 19:46:02 -0300

    Subindo Projeto com Spring

---
+++ src/test/java/com/threeway/curso/boot/CadastroFuncionariosApplicationTests.java
@@ -0,0 +1,16 @@
+package com.threeway.curso.boot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CadastroFuncionariosApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
